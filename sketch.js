e337e50a1b76f6083216b6965c87f9d5e5b319eb
bhushananika <73623373+bhushananika@users.noreply.github.com>
2020-10-30 20:18:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle,obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  monkey=createSprite(80,315,20,20);
+  monkey.addAnimation("moving", monkey_running);
+  monkey.scale=0.1
+  
+  ground=createSprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x);
+}
+
+
+function draw() {
+background(225);
+  if(ground.x<0){
+    ground.x=ground.width/2;
+ }
+  if(keyDown("space")){
+    monkey.velocityY=-12;
+  }
+  monkey.velocityY=monkey.velocityY+0.8;
+  monkey.collide(ground);
+  obstacleGroup();
+  FoodGroup();
+  drawSprites();
+  
+}
+
+function FoodGroup(){
+  if(frameCount%80===0){
+  var banana=createSprite(200,200);
+    banana.y=Math.round(random(120,200));
+    banana.velocityX=-5
+    banana.addImage(bananaImage)
+    banana.lifetime=500
+    banana.scale=0.1
+  }
+}
+
+function obstacleGroup(){
+  if(frameCount%300===0){
+  var obstacle=createSprite(200,200);
+    obstacle.velocityX=-5
+    obstacle.lifetime=500
+  }
+}
+
+
+
+
+
